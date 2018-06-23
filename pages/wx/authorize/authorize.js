a247f77ba14f4609091312a33ce60f860f0531cb
mz_zml <mz_zml@163.com>
2018-06-24 01:56:49 +0800

    ST FIX

--- pages/wx/authorize/authorize.js
+++ pages/wx/authorize/authorize.js
@@ -11,6 +11,7 @@ Page({
   data: {
   
   },
+  timeout: null,
   interval: null,
   redirectUrl: '',
   tryAgain: true,
@@ -71,7 +72,7 @@ Page({
               app.globalData.userAttribute = res.data.userAttribute
               wx.setStorageSync('userAttribute', res.data.userAttribute)
 
-              clearTimeout(this.timeout)
+              // this.timeout && clearTimeout(this.timeout)
               wx.reLaunch({
                 url: '../../../' + that.redirectUrl
               })
